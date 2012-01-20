c2782c44a2eb995189aafc392e318b5f6ecb5aac
alphalemon <g.diblas@email.it>
2012-01-20 12:44:42 +0100

    added controller for the frontend bundle

---
+++ src/Acme/WebSiteBundle/Controller/WebSiteController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Acme\WebSiteBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use AlphaLemon\FrontendBundle\Controller\FrontendController;
+
+class WebSiteController extends FrontendController
+{
+}
