f6f1b5e6d569e142f37ebc042488e5e5b2d437fe
alphalemon <g.diblas@email.it>
2012-02-08 18:27:59 +0100

    fixed defaut dsn in config.php

--- bin/config.php
+++ bin/config.php
@@ -1,7 +1,7 @@
 <?php
     $companyName = "Acme";
     $bundleName = "WebSiteBundle";
-    $dsn = "mysql:host=localhost";
+    $dsn = "mysql:host=localhost;dbname=alphalemon";
     $database = "alphalemon";
     $user = "root";
     $password = "";
