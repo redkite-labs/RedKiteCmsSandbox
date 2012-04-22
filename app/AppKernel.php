f5672f6a4d6b9d4f4479d15b40d952093057b610
alphalemon <g.diblas@email.it>
2012-04-22 18:52:03 +0300

    added bootstrap bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
+            new AlphaLemon\BootstrapBundle\AlphaLemonBootstrapBundle(),
             new Acme\WebSiteBundle\AcmeWebSiteBundle(),
         );
 
