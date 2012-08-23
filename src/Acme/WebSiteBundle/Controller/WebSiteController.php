8bfa74c904ff8d04c150b81c481819d1547b3025
alphalemon <g.diblas@email.it>
2012-08-23 18:41:47 +0200

    updated to use FrontendBundle managed by theme engine

--- src/Acme/WebSiteBundle/Controller/WebSiteController.php
+++ src/Acme/WebSiteBundle/Controller/WebSiteController.php
@@ -5,7 +5,7 @@ namespace Acme\WebSiteBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
-use AlphaLemon\FrontendBundle\Controller\FrontendController;
+use AlphaLemon\ThemeEngineBundle\Core\Rendering\Controller\FrontendController;
 
 class WebSiteController extends FrontendController
 {
