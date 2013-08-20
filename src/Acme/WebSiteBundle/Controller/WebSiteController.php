209400986b9504eea69db4230e067d98326c6aca
RedKite Labs <info@redkite-labs>
2013-08-20 19:24:32 +0200

    fixed namespace

--- src/Acme/WebSiteBundle/Controller/WebSiteController.php
+++ src/Acme/WebSiteBundle/Controller/WebSiteController.php
@@ -5,7 +5,7 @@ namespace Acme\WebSiteBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
-use AlphaLemon\ThemeEngineBundle\Core\Rendering\Controller\FrontendController;
+use RedKiteLabs\ThemeEngineBundle\Core\Rendering\Controller\FrontendController;
 
 class WebSiteController extends FrontendController
 {
