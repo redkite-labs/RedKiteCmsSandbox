37451392ec76cd96641f4da6ed3cb6e4f1529715
fatmah <fatmahedfi@gmail.com>
2014-05-30 08:54:57 -0700

    Update AppKernel.php

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,7 +16,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new RedKiteCms\InstallerBundle\RedKiteCmsInstallerBundle(),
+            new RedKiteLabs\RedKiteCms\InstallerBundle\RedKiteCmsInstallerBundle(),
             new Acme\WebSiteBundle\AcmeWebSiteBundle(),
         );
 
